903614f03aa449be95ca9f18003379d2e488b875
AdelR1994 <a.ribeiro@leadertread.co.za>
2025-06-08 19:02:05 +0200

    Add comments to key functions for better readability

---
+++ helloWorld_task3.py
@@ -0,0 +1,8 @@
+# Be consistent with renaming any variables
+# Prompt the user for input
+# Change "some text" and "user_input" to usefull data
+user_name = input("Please enter your name: ")
+
+# Print out what the user entered
+# Make sure to update print message when changing variables
+print("Your name is:", user_name)
